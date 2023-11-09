b2316f41c1843cfbcb72d79d80a3baff33138d04
Runek00 <runek00@gmail.com>
2023-11-09 23:22:51 +0100

    first signs of some sort of gui

--- webGui.go
+++ webGui.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
 func webGui() {
-	// TODO
+	http.HandleFunc("/", mainPage)
+
+	log.Fatal(http.ListenAndServe(":8079", nil))
+}
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("static/templates/index.html"))
+	tmpl.Execute(w, nil)
 }
