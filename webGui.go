e49d0a587d7c679bfd77bf820ed26d3e16875108
Runek00 <runek00@gmail.com>
2023-11-16 20:54:56 +0100

    somewhat working example

--- webGui.go
+++ webGui.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -8,11 +9,35 @@ import (
 
 func webGui() {
 	http.HandleFunc("/", mainPage)
+	http.HandleFunc("/pullAll", pullAllHandler)
+	http.HandleFunc("/pullEvents", pullEvents)
 
-	log.Fatal(http.ListenAndServe(":8079", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("static/templates/index.html"))
 	tmpl.Execute(w, nil)
+}
+
+func pullAllHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.New("sse-element")
+	tmpl.Parse(`<div id="yolo" name="sse" hx-ext="sse" sse-connect="/pullEvents" sse-swap="message">`)
+	tmpl.Execute(w, nil)
+}
+
+func pullEvents(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Content-Type", "text/event-stream")
+	statusChan := make(chan string)
+
+	go func() {
+		for status := range statusChan {
+			fmt.Fprintf(w, "data: "+status)
+			w.(http.Flusher).Flush()
+		}
+	}()
+	PullAll(statusChan)
 }
